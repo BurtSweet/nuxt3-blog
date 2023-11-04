31c3eb51141940476112560d1d597dcd6e7998d8
BurtSweet <105471886+BurtSweet@users.noreply.github.com>
2023-11-04 11:28:44 +0800

    Update index.vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -8,11 +8,8 @@ const commitUrl = computed(() => `https://github.com/${config.githubName}/${conf
 const buildTime = ref<string>("$(inject:timestamp)");
 
 const paragraphs = [
-  "幽深宇宙已岁逾百亿，惟闪烁星光点缀生机",
-  "我常仰望浩瀚天际，思念在同一颗星球的你",
-  "想，那转瞬的迷人流星，也许就是你的回眸",
-  "光坠之地，吾之忧祈",
-  "——2021.12.4"
+  "胸怀鸿鹄之志而生如蝼蚁",
+  "虽有不屈之心却命比纸"
 ];
 
 onBeforeMount(async () => {
