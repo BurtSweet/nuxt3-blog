97b756a50c0279dd11d13827d885cff5257ddc25
BurtSweet <105471886+BurtSweet@users.noreply.github.com>
2025-02-15 10:40:01 +0800

    大版本更新

--- config.ts
+++ config.ts
@@ -17,22 +17,17 @@ export default {
   CommentRepoId: "",                                           // https://giscus.app/zh-CN
   CommentDiscussionCategoryId: "",                             // https://giscus.app/zh-CN
 
-  Comment: {                                                   // 是否开启评论，请先设置上面的 CommentRepoId 和 CommentDiscussionCategoryId
-    articles: true,                                            // “文章”是否开启评论
-    records: false,                                            // “记录”是否开启评论
-    knowledges: false                                          // “文化”是否开启评论
-  },
   MongoDb: {                                                   // 浏览量统计，请先设置 https://vercel.com/integrations/mongodbatlas
+    database: "nuxt3-blog",
+    collection: "visitors",
     initialVisitors: 1,                                        // 如果设置成10000，那么发一篇文章立马就有10000个浏览量！
     visitFromOwner: false                                      // 网站拥有者访问时，是否增加浏览量
   },
-  themeColor: "#2aa0bb",                                       // 主题色
+  themeColor: "#2aa0bb",                                     // 主题色
   defaultLang: "zh",                                           // default language, "zh" and "en" are supported currently
-  timezone: "Asia/Shanghai",                                   // timezone, please refer to https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
 
 
+  /** -------------------------------------------------注意----------------------------------------------------- */
 
-  /** -------------------------------------------------以下不可修改----------------------------------------------------- */
-
-  githubRepo: "nuxt3-blog"                                    // 勿修改
+  githubRepo: "nuxt3-blog"                                    // 需要与仓库名一致，如果fork时更改了仓库名，则这里也要改
 };
